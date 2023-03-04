0f8fdecd75880b3a43437ae0c6ac3bf8747fdbe1
ernest <pinedaernest0818@gmail.com>
2023-03-04 19:52:19 +0800

    fix

--- src/config/index.vue
+++ src/config/index.vue
@@ -89,7 +89,7 @@ export default {
     <el-header>
       <el-row :gutter="10">
         <el-col :xs="8" :sm="6" :md="4" :lg="3" >
-          <el-select v-model="method" placeholder="Select" style="width: 100%">
+          <el-select v-model="method" placeholder="Method" style="width: 100%">
             <el-option
                 v-for="item in httpMethods"
                 :label="item.label"
@@ -98,7 +98,7 @@ export default {
           </el-select>
         </el-col>
         <el-col :xs="8" :sm="6" :md="4" :lg="3" >
-          <el-select v-model="logLevel" placeholder="Select" style="width: 100%">
+          <el-select v-model="logLevel" placeholder="Log Level" style="width: 100%">
             <el-option
                 v-for="item in logLevels"
                 :label="item.label"
